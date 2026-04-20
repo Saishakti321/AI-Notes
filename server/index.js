f7ceebd6f39ca46f291d0d5b24f8fd1535099bf0
Saishakti Tripathy <saishaktitripathy321@gmail.com>
2026-04-21 00:47:46 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ stripeWebhook
 
 
 app.use(cors(
-    {origin:"http://localhost:5173" ,
+    {origin:"https://ai-notes-clientside.onrender.com" ,
     credentials:true,
     methods:["GET","POST","PUT","DELETE","OPTIONS"]
    }
